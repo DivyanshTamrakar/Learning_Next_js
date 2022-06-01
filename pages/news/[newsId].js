9546a523b4e2c0731baa13f421b58639301ec754
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-02 02:01:32 +0530

    going forward for the project

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,15 +1,19 @@
 import { useRouter } from 'next/router'
-
+import Link from "next/link";
 
 //out domain/news/details
 
 
 function Details() {
-const router = useRouter()
-const newsId = router.query.newsId
+  const router = useRouter()
+  const newsId = router.query.newsId
 
   return (
-    <h1>This is Detail Page </h1>
+    <div>
+      <h1>This is Detail Page </h1>
+      <Link href="/news" >Back to news page or previous page </Link>
+      <Link href="/" >Back to home </Link>
+    </div>
   )
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -10,7 +10,17 @@ function News() {
                 <Link href="/news/divyansh"><li>
                     NextJs is great framework , hottest in the industry
                     </li></Link>
+                <Link href='/news/tamrakar'>
                 <li>Typescript is staic type checking library for javascript</li>
+                </Link>
+                {/* <Link
+          href={{
+            pathname: '/about',
+            query: { name: 'test' },
+          }}
+        >
+            navigate with object
+        </Link> */}
             </ul>
         </div>
     )
